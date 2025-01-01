75792db67f9f680e549668ffdfe645acc6ee58f6
caribouHY <oririyz@gmail.com>
2025-01-01 09:38:38 +0000

    feat: new templates

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # sir_templates
 TextFSM templates for  parsing show commands of Si-R router, SR-S switch
+
+Si-R, SR-S用のTextFSMテンプレートです。
+
+## 開発用スクリプト
+
+リポジトリのルートディレクトリ上で実行します。
+```
+テストデータの実行結果の出力
+python3 dev.py conv [file or dir]
+
+テストデータの実行結果をyamlファイルに保存
+python3 dev.py gen [file or dir]
+```
---
+++ dev.py
@@ -0,0 +1,92 @@
+import sys
+from pathlib import Path
+
+import textfsm
+import yaml
+
+
+def read_text(path: str) -> str:
+    with open(path) as f:
+        return f.read()
+
+
+def parse_text(path: str, text: str) -> list:
+    with open(path) as f:
+        template = textfsm.TextFSM(f)
+        result = template.ParseTextToDicts(text)
+        return [{key.lower(): value for key, value in d.items()} for d in result]
+
+
+def write_yaml(path: str, parsed_list: list):
+    with open(path, mode="w", encoding="utf-8") as f:
+        yaml.safe_dump({"parsed_sample": parsed_list}, f)
+
+
+def convert_yaml(template_path, raw_path, output_path=None, console=False):
+    result = parse_text(template_path, read_text(raw_path))
+    if console:
+        print(raw_path)
+        print(yaml.dump(result))
+    if output_path is not None:
+        write_yaml(output_path, result)
+        print(f"generated: {output_path}")
+
+
+def generation_yaml_form_file(path: str, write: bool = True):
+    input_path = Path(path)
+    output_path = input_path.with_suffix(".yml").as_posix()
+    command = input_path.parent.name
+    vendor = input_path.parent.parent.name
+    template_path = f"./templates/{vendor}_{command}.textfsm"
+
+    if write:
+        convert_yaml(template_path, path, output_path, False)
+    else:
+        convert_yaml(template_path, path, None, True)
+
+
+def generation_yaml_form_dir(path: str, write: bool = True):
+    input_path = Path(path)
+    command = input_path.name
+    vendor = input_path.parent.name
+    template_path = f"./templates/{vendor}_{command}.textfsm"
+    raw_files = list(input_path.glob("*.raw"))
+
+    for raw_file in raw_files:
+        if write:
+            output_path = raw_file.with_suffix(".yml").as_posix()
+            convert_yaml(template_path, raw_file.as_posix(), output_path, False)
+        else:
+            convert_yaml(template_path, raw_file.as_posix(), None, True)
+
+
+def generation_yaml(path: str, write: bool = True):
+    input_path = Path(path)
+    if input_path.is_file():
+        generation_yaml_form_file(path, write)
+    else:
+        generation_yaml_form_dir(path, write)
+
+
+def main():
+    args = sys.argv
+    if len(args) < 2:
+        print("subcommand")
+        return
+
+    if args[1] == "gen":
+        if len(args) == 3:
+            generation_yaml(args[2])
+        else:
+            print("invailed paramater")
+    elif args[1] == "conv":
+        if len(args) == 3:
+            generation_yaml(args[2], False)
+        else:
+            print("invailed paramater")
+    else:
+        print("invailed subcommand")
+
+
+if __name__ == "__main__":
+    main()
